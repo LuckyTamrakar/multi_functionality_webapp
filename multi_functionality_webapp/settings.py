dbd36c81dd7a52a35fdd33464b00e5607cd05d0b
Tamrakar Lucky <luckytamrakar.01@gmail.com>
2022-10-14 21:23:03 -0700

    Initial djnago

--- multi_functionality_webapp/settings.py
+++ multi_functionality_webapp/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'multi_functionality_webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'multi_functionality',
-        'USER': 'root',
-        'PASSWORD': '7024197711Lt@',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
